b7f03f1dea78ea9d276924c194dfa09fd2056bb2
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-05 15:36:35 +0200

    authentication controlers, routes and middlewares

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoute from './routes/authRoute.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/user', authRoute);
 
 app.get('/',(req,res)=>{
     res.send("Express server is running")
