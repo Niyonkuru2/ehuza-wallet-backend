3e83918979eb19f4e39850eef7d93e9fbc920bdc
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-06 15:08:29 +0200

    server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoute from './routes/authRoute.js';
+import walletRoute from './routes/walletRoute.js';
+import transactionRoute from './routes/transactionRoutes.js';
 
 dotenv.config();
 
@@ -9,7 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/user', authRoute);
+app.use('/api/wallet', walletRoute);
+app.use('/api/transactions', transactionRoute);
 
 app.get('/',(req,res)=>{
     res.send("Express server is running")
