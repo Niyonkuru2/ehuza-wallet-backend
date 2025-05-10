c8e5367b903ce7e72ff73fa6540aa7012bbd7b1f
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-10 09:56:46 +0200

    authuser in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authUser from './middleware/AuthMiddleware.js';
 import walletRoute from './routes/walletRoute.js';
 import transactionRoute from './routes/transactionRoutes.js';
 
@@ -10,8 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/wallet', walletRoute);
-app.use('/api/transactions', transactionRoute);
+app.use('/user', authUser);
+app.use('/wallet', walletRoute);
+app.use('/transactions', transactionRoute);
 
 app.get('/',(req,res)=>{
     res.send("Express server is running")
