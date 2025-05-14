6296e866679b44734fd9470138285ad4748c6f6f
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-14 10:40:08 +0200

    server.js

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
 
 
 app.use('/user', authRoute);
